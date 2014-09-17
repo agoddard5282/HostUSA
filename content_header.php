73e7a67a73150a2a5c2fb2053cc5267fdb8d0f5f
anthony <agoddard@axcess-financial.com>
2014-09-16 21:21:08 -0400

    deleted site

--- content_header.php
+++
@@ -1,7 +0,0 @@
-<ul class="columnNavigation">
-<li><a class="iconSmallPhone" href="#"><strong><?php echo $phonenumber; ?></strong></a></li>
-<li><a class="iconSmallChat" href="./company-blog.php"><strong>Company</strong> Blog</a></li>
-<li><a class="iconSmallEmail" href="/clients/submitticket.php"><strong>Submit</strong> Ticket</a></li>
-<li><a class="iconSmallClient" href="./client-login.php"><strong>Client</strong> Login</a></li>
-</ul>
-
--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-$phonenumber = "(937) 217-7557";
-
-// Require https
-$url = 'https://www.hostusa.us';
-?>
